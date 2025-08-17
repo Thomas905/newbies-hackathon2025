5dacf30b18f9ab6229f4af69430095206c365eed
Thomas905 <Thomas905@users.noreply.github.com>
2025-08-17 11:51:16 +1000

    Exit Game with Q and ESCAPE (Keyboard)

--- game.py
+++ game.py
@@ -348,6 +348,8 @@ class GameArea:
                         if current_time - last_shoot_time > shoot_cooldown:
                             player.shoot()
                             last_shoot_time = current_time
+                    if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                        running = False
             
             if ControlMode.HAND and detector.is_grab:
                 if current_time - last_shoot_time > shoot_cooldown:
